b5b76e56eba00f38b3a88ef85ae267af308af60a
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-09-07 23:24:40 +0200

    Retrieve all sessions using a GET request in JSON format

--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
 from .serializers import UserProfileSerializer, SessionSerializer, TaskSerializer, SubmissionSerializer, ActivityLogSerializer
 from .models import UserProfile, Session, Submission, Task, ActivityLog
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+def sessions(request):
+  if request.method == 'GET':
+    sessions = Session.objects.all()
+    serializer = SessionSerializer(sessions, many=True)
+    return JsonResponse({"sessions": serializer.data}, safe=False)
