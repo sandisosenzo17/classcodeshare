6059d6f3d72cca80565e071be819150cca623d9d
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-09-07 23:29:28 +0200

    Extend to a POST request and handle request status

--- classroom/views.py
+++ classroom/views.py
@@ -4,6 +4,7 @@ from .models import UserProfile, Session, Submission, Task, ActivityLog
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def sessions(request):
@@ -11,3 +12,9 @@ def sessions(request):
     sessions = Session.objects.all()
     serializer = SessionSerializer(sessions, many=True)
     return JsonResponse({"sessions": serializer.data}, safe=False)
+  elif request.method == 'POST':
+    serializer = SessionSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
